e864bf77f8f33baeac6b14cb524835a802dd45dc
Paul Storkman <storkman@storkman.nl>
2023-03-08 04:49:56 +0100

    Reduce copy-paste in the self-tests.

--- self_tests.c
+++ self_tests.c
@@ -14,64 +14,55 @@
 #define DH_IMPLEMENT_HERE
 #include "dh_cuts.h"
 
+static int
+spawn_test(const char *description, int should_crash)
+{
+	pid_t pid;
+	int test_status;
+	dh_push("%s", description);
+	pid = fork();
+	if (pid == 0) {
+		dh_init(fopen("/dev/null", "w"));
+		return 0;
+	}
+	waitpid(pid, &test_status, 0);
+	dh_assert(! (should_crash && test_status == 0));
+	dh_assert(! ((!should_crash) && test_status != 0));
+	dh_pop();
+	return 1;
+}
+
 static void
 test_crash_recovery(void)
 {
-	dh_push("crash recovery");
-	pid_t pid = fork();
-	if (pid == 0) {
-		dh_init(fopen("/dev/null", "w"));
-		dh_branch( raise(SIGBUS); )
-		exit(0);
-	}
-	if (pid > 0) {
-		int ret = 0;
-		waitpid(pid, &ret, 0);
-		dh_assert(ret == 0);
-	}
-	dh_pop();
+	if (spawn_test("crash recovery", 0))
+		return;
+	dh_branch( raise(SIGBUS); )
+	exit(0);
 }
 
 static void
 test_forfeit_on_failure(void)
 {
-	dh_push("forfeit on failure");
-	pid_t pid = fork();
-	if (pid == 0) {
-		dh_init(fopen("/dev/null", "w"));
-		dh_throw("always throw");
-		exit(0);
-	}
-	if (pid > 0) {
-		int ret = 0;
-		waitpid(pid, &ret, 0);
-		dh_assert(ret != 0);
-	}
-	dh_pop();
+	if (spawn_test("forfeit on failure", 1))
+		return;
+	dh_throw("always throw");
+	exit(0);
 }
 
 static void
 test_stack_overflow(void)
 {
 	int i;
-	dh_push("stack overflow");
-	pid_t pid = fork();
-	if (pid == 0) {
-		dh_init(fopen("/dev/null", "w"));
-		dh_branch(
-			for (i = 0; i < DH_MAX_DEPTH * 10; i++)
-				dh_push("step %d", i + 1);
-			for (i = 0; i < DH_MAX_DEPTH * 10; i++)
-				dh_pop();
-		)
-		exit(0);
-	}
-	if (pid > 0) {
-		int ret = 0;
-		waitpid(pid, &ret, 0);
-		dh_assert(ret == 0);
-	}
-	dh_pop();
+	if (spawn_test("stack overflow", 0))
+		return;
+	dh_branch(
+		for (i = 0; i < DH_MAX_DEPTH * 10; i++)
+			dh_push("step %d", i + 1);
+		for (i = 0; i < DH_MAX_DEPTH * 10; i++)
+			dh_pop();
+	)
+	exit(0);
 }
 
 int
